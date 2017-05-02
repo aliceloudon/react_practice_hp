bd7ec9b6e892011bbca5fe65a6dae3659446a4d7
David Hale <dhale12@hotmail.co.uk>
2017-05-02 13:26:41 +0100

    made render and constructor for characters container

--- client/src/containers/CharactersContainer.jsx
+++ client/src/containers/CharactersContainer.jsx
@@ -2,8 +2,27 @@ import React from 'react'
 
 class CharactersContainer {
 
-  
-  
+  constructor(props) {
+    super(props)
+    this.state = {
+      characters: [],
+      selectedCharacter: null
+    }
+  }
+
+
+  render() {
+
+    return(
+      <div>
+        <h2>Dave and Alice's Great Harry Potter Page</h2>
+        <CharacterSelector />
+        <CharacterDetail />
+      </div>
+    )
+
+  }
+
 }
 
 export default CharactersContainer
